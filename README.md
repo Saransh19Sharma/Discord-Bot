e7acf98dbd1c5f3c55e66de8a52ca202babb905a
Saransh <saranshsharma1919@gmail.com>
2022-07-26 15:35:07 +0530

    Nothing

---
+++ README.md
@@ -0,0 +1,3 @@
+Created By - Saransh Sharma
+Email Id - saranshsharma1919@gmail.com
+For any query join my Discord Server - "https://discord.gg/WA6Jpdtz3A"
--- tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-@client.command(name='join', help='Tells the bot to join the voice channel')
-# async def join(ctx):
-#     if not ctx.message.author.voice:
-#         await ctx.send("{} is not connected to a voice channel".format(ctx.message.author.name))
-#         return
-#     else:
-#         channel = ctx.message.author.voice.channel
-#     await channel.connect()
